1e9e9c28d84d0946573564e667a26eebb48d094c
Nuna Razvan <nuna_is_washed_up@yahoo.com>
2019-03-06 11:19:08 +0200

    Added Fractii

---
+++ Fractii/Fractii/Fractie.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Fractii
+{
+    internal class Fractie
+    {
+        private int numarator, numitor;
+
+        public Fractie()
+        {
+            numarator = 0;
+            numitor = 1;
+        }
+
+        public Fractie(int numarator, int numitor)
+        {
+            int d;
+
+            d = gcd(numarator, numitor);
+            this.numarator = numarator / d;
+            this.numitor = numitor / d;
+        }
+
+        public Fractie(string s)
+        {
+            int x = 0, y = 0, i = 0, d;
+
+            while (s[i] >= '0' && s[i] <= '9')
+            {
+                x = x * 10 + s[i] - '0';
+                i++;
+            }
+            while (s[i] < '0' || s[i] > '9')
+                i++;
+            while (i < s.Length && s[i] >= '0' && s[i] <= '9')
+            {
+                y = y * 10 + s[i] - '0';
+                i++;
+            }
+
+            d = gcd(x, y);
+            numarator = x / d;
+            numitor = y / d;
+        }
+
+        public Fractie Add(Fractie right)
+        {
+            int r_numarator, r_numitor;
+
+            r_numitor = numitor * right.numitor;
+            r_numarator = numarator * right.numitor + numitor * right.numarator;
+
+            return new Fractie(r_numarator, r_numitor);
+        }
+
+        public Fractie Subtract(Fractie right)
+        {
+            int r_numarator, r_numitor;
+
+            r_numitor = numitor * right.numitor;
+            r_numarator = numarator * right.numitor - numitor * right.numarator;
+
+            return new Fractie(r_numarator, r_numitor);
+        }
+
+        public Fractie Multiply(Fractie right)
+        {
+            int r_numarator, r_numitor;
+
+            r_numitor = numitor * right.numitor;
+            r_numarator = numarator * right.numarator;
+
+            return new Fractie(r_numarator, r_numitor);
+        }
+
+        public Fractie Divide(Fractie right)
+        {
+            int r_numarator, r_numitor;
+
+            r_numitor = numitor * right.numarator;
+            r_numarator = numarator * right.numitor;
+
+            return new Fractie(r_numarator, r_numitor);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(numarator + " / " + numitor);
+        }
+
+        public static int gcd(int a, int b)
+        {
+            int r = a % b;
+
+            while (r != 0)
+            {
+                a = b;
+                b = r;
+                r = a % b;
+            }
+
+            return b;
+        }
+
+    }
+}
---
+++ Fractii/Fractii/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fractie f1 = new Fractie();
+            Fractie f2 = new Fractie(3, 9);
+            Fractie f3 = new Fractie("4 / 8");
+            Fractie f4;
+
+            f1.Print();
+            f2.Print();
+            f3.Print();
+
+            f4 = f2.Add(f3);
+            f4.Print();
+
+            f4 = f2.Subtract(f3);
+            f4.Print();
+
+            f4 = f2.Multiply(f3);
+            f4.Print();
+
+            f4 = f2.Divide(f3);
+            f4.Print();
+        }
+    }
+}
+
